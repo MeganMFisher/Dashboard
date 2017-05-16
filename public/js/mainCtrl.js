e7f377ba93a051c24958436e547166c178338f91
MeganMFisher <meganmichelle318@gmail.com>
2017-05-15 23:03:52 -0600

    vistor stats complete

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -24,20 +24,31 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
              console.log(visitors)
             let monthVisitors = []
             visitors.map((e) => {
-
                 let visitorDate = e.day.split('-')
                 if(visitorDate[1] === '05'){
                   monthVisitors.push(e)
                 }
-        })
-           
-            // $scope.visitors = monthVisitors
+            })
             let visitorsTotal = 0
             monthVisitors.map((e) =>{
                 visitorsTotal += e.visitors
             })
-            console.log(visitorsTotal)
-            return visitorsTotal
+            $scope.visitors = visitorsTotal
+            console.log($scope.visitors)
+
+            let pageVisits = 0
+            monthVisitors.map((e) =>{
+                pageVisits += e.pagevisits
+            })
+            $scope.pageVisit = pageVisits
+            console.log($scope.pageVisit)
+
+            let knownContacts = 0
+            monthVisitors.map((e) =>{
+                knownContacts += e.knowncontacts
+            })
+            $scope.contacts = knownContacts
+            console.log($scope.contacts)
         })
     }
     getVisitors()
