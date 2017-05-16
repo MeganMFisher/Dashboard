dc0d3057e434d3963e4100a99be66296bb848f81
MeganMFisher <meganmichelle318@gmail.com>
2017-05-16 12:10:16 -0600

    visitors and payments month selected working

--- public/js/controllers/payments.js
+++ public/js/controllers/payments.js
@@ -4,36 +4,33 @@ angular.module('app').controller('paymentsCtrl', function ($scope, mainSrv) {
     // *           Payments Received             *
     // *******************************************
 
-    let getPaymentInfo = () => {
+    let getPaymentInfo = (monthSelected) => {
         mainSrv.getPaymentInfo().then((res) => {
             let payments = res.data
             let monthlyPayments = []
             payments.map((e) => {
                 let paymentDate = e.day.split('-')
-                if(paymentDate[1] === '05') {
+                if(paymentDate[1] === monthSelected) {
                     monthlyPayments.push(e)
                 }
             })
 
+            console.log(monthlyPayments)
 
             let paypalTotals = 0
-            monthlyPayments.map((e) => {
-                paypalTotals += e.paypaltotal
-
-            })
-            console.log(paypalTotals)
-
             let ccTotals = 0
             monthlyPayments.map((e) => {
+                paypalTotals += e.paypaltotal
                 ccTotals += e.cctotal
+
             })
-            console.log(ccTotals)
 
             let totalPayments = paypalTotals  + ccTotals
-            console.log(totalPayments)
+            $scope.percentPaypal = paypalTotals / totalPayments * 100
+            $scope.percentCC = ccTotals / totalPayments * 100
         })
     }
-    getPaymentInfo()
+    getPaymentInfo('05')
 
 
 })
--- public/js/controllers/visitorsCtrl.js
+++ public/js/controllers/visitorsCtrl.js
@@ -6,39 +6,31 @@
     // *******************************************
 
  
-    let getVisitors = () => {
+    let getVisitors = (monthSelected) => {
         mainSrv.getVisitors().then((res) => {
              let visitors = res.data
-            //  console.log(visitors)
+             console.log(visitors)
             let monthVisitors = []
             visitors.map((e) => {
                 let visitorDate = e.day.split('-')
-                if(visitorDate[1] === '05'){
+                if(visitorDate[1] === monthSelected){
                   monthVisitors.push(e)
                 }
             })
             let visitorsTotal = 0
+            let pageVisits = 0
+            let knownContacts = 0
+            
             monthVisitors.map((e) =>{
                 visitorsTotal += e.visitors
-            })
-            $scope.visitors = visitorsTotal
-            // console.log($scope.visitors)
-
-            let pageVisits = 0
-            monthVisitors.map((e) =>{
                 pageVisits += e.pagevisits
-            })
-            $scope.pageVisit = pageVisits
-            // console.log($scope.pageVisit)
-
-            let knownContacts = 0
-            monthVisitors.map((e) =>{
                 knownContacts += e.knowncontacts
             })
+            $scope.visitors = visitorsTotal
+            $scope.pageVisit = pageVisits
             $scope.contacts = knownContacts
-            // console.log($scope.contacts)
         })
     }
-    getVisitors()
+    getVisitors('05')
 
 })
