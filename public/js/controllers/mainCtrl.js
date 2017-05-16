3d743ba3e309f3a79e719a1906b47daf36a49b2d
MeganMFisher <meganmichelle318@gmail.com>
2017-05-16 08:38:40 -0600

    seperated visitors ctrl

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -13,48 +13,6 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
     $scope.getSalesInfo()
 
 
-    // *******************************************
-    // *            Website Visitors             *
-    // *******************************************
-
-
-    let getVisitors = () => {
-        mainSrv.getVisitors().then((res) => {
-             let visitors = res.data
-             console.log(visitors)
-            let monthVisitors = []
-            visitors.map((e) => {
-                let visitorDate = e.day.split('-')
-                if(visitorDate[1] === '05'){
-                  monthVisitors.push(e)
-                }
-            })
-            let visitorsTotal = 0
-            monthVisitors.map((e) =>{
-                visitorsTotal += e.visitors
-            })
-            $scope.visitors = visitorsTotal
-            console.log($scope.visitors)
-
-            let pageVisits = 0
-            monthVisitors.map((e) =>{
-                pageVisits += e.pagevisits
-            })
-            $scope.pageVisit = pageVisits
-            console.log($scope.pageVisit)
-
-            let knownContacts = 0
-            monthVisitors.map((e) =>{
-                knownContacts += e.knowncontacts
-            })
-            $scope.contacts = knownContacts
-            console.log($scope.contacts)
-        })
-    }
-    getVisitors()
-
-
-
 
 
     // *******************************************
---
+++ public/js/controllers/visitorsCtrl.js
@@ -0,0 +1,44 @@
+ 
+ angular.module('app').controller('visitorsCtrl', function ($scope, mainSrv) {
+
+    // *******************************************
+    // *            Website Visitors             *
+    // *******************************************
+
+ 
+    let getVisitors = () => {
+        mainSrv.getVisitors().then((res) => {
+             let visitors = res.data
+             console.log(visitors)
+            let monthVisitors = []
+            visitors.map((e) => {
+                let visitorDate = e.day.split('-')
+                if(visitorDate[1] === '05'){
+                  monthVisitors.push(e)
+                }
+            })
+            let visitorsTotal = 0
+            monthVisitors.map((e) =>{
+                visitorsTotal += e.visitors
+            })
+            $scope.visitors = visitorsTotal
+            console.log($scope.visitors)
+
+            let pageVisits = 0
+            monthVisitors.map((e) =>{
+                pageVisits += e.pagevisits
+            })
+            $scope.pageVisit = pageVisits
+            console.log($scope.pageVisit)
+
+            let knownContacts = 0
+            monthVisitors.map((e) =>{
+                knownContacts += e.knowncontacts
+            })
+            $scope.contacts = knownContacts
+            console.log($scope.contacts)
+        })
+    }
+    getVisitors()
+
+})
