f5afcf2ee47fd3339cc01663cd8cd4ee14562cf9
MeganMFisher <meganmichelle318@gmail.com>
2017-05-16 21:14:48 -0600

    payments service done!

--- public/js/mainSrv.js
+++ public/js/mainSrv.js
@@ -3,16 +3,6 @@ angular.module('app').service('mainSrv', function ($http) {
   let devUrl = 'http://localhost:3500'
 
 
-  // // *******************************************
-  // // *            Website Visitors             *
-  // // *******************************************
-
-  // this.getVisitors = () => {
-  //   return $http.get(devUrl + '/visitors').then((res) => {
-  //     return res
-  //   })
-  // }
-
   // *******************************************
   // *           Product Type Sold             *
   // *******************************************
@@ -28,10 +18,10 @@ angular.module('app').service('mainSrv', function ($http) {
   // *           Product Inventory             *
   // *******************************************
 
-  this.getPaymentInfo = () => {
-    return $http.get(devUrl + '/payment').then((res) => {
-      return res
-    })
-  }
+  // this.getPaymentInfo = () => {
+  //   return $http.get(devUrl + '/payment').then((res) => {
+  //     return res
+  //   })
+  // }
 
 })
--- public/js/services/paymentsSrv.js
+++ public/js/services/paymentsSrv.js
@@ -1,34 +1,40 @@
-angular.module('app').controller('paymentsCtrl', function ($scope, mainSrv) {
+angular.module('app').service('paymentsSrv', function ($http) {
+
+  let devUrl = 'http://localhost:3500'
 
     // *******************************************
     // *           Payments Received             *
     // *******************************************
 
-    let getPaymentInfo = (monthSelected) => {
-        mainSrv.getPaymentInfo().then((res) => {
-            let payments = res.data
+  this.getPaymentInfo = () => {
+    return $http.get(devUrl + '/payment').then((res) => {
+      return res
+    })
+  }
+
+
+   this.paymentInfo = (res, month) => {
+    let payments = res.data
             let monthlyPayments = []
+            let paymentNums = []
             payments.map((e) => {
                 let paymentDate = e.day.split('-')
-                if(paymentDate[1] === monthSelected) {
+                if(paymentDate[1] === month) {
                     monthlyPayments.push(e)
                 }
             })
-
             let paypalTotals = 0
             let ccTotals = 0
             monthlyPayments.map((e) => {
                 paypalTotals += e.paypaltotal
                 ccTotals += e.cctotal
-
             })
-
             let totalPayments = paypalTotals  + ccTotals
-            $scope.percentPaypal = paypalTotals / totalPayments * 100
-            $scope.percentCC = ccTotals / totalPayments * 100
-        })
+            paymentNums.push(Math.round(ccTotals / totalPayments * 100))
+            paymentNums.push(Math.round(paypalTotals / totalPayments * 100))
+            console.log(paymentNums)
+            return paymentNums
     }
-    getPaymentInfo('05')
 
 
 })
