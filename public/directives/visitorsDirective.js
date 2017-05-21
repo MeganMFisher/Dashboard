483294d8e44764bfc2db94605ed48f408fa246e1
MeganMFisher <meganmichelle318@gmail.com>
2017-05-21 11:53:14 -0600

    bars are showing!

--- public/directives/visitorsDirective.js
+++ public/directives/visitorsDirective.js
@@ -9,34 +9,41 @@ angular.module('app')
       controller: function ($scope) {
 
         $scope.visitorsData = [
-          {day: 1, visitors: 100},
-          {day: 2, visitors: 100},
-          {day: 3, visitors: 100},
-          {day: 4, visitors: 100},
-          {day: 5, visitors: 100},
-          {day: 6, visitors: 100},
-          {day: 1, visitors: 100}
+          { day: 1, visitors: 100 }, 
+          { day: 2, visitors: 250 }, 
+          { day: 3, visitors: 175 }, 
+          { day: 4, visitors: 200 },
+          { day: 5, visitors: 100 }, 
+          { day: 6, visitors: 250 }, 
+          { day: 7, visitors: 175 }, 
+          { day: 8, visitors: 200 },
+          { day: 9, visitors: 100 }, 
+          { day: 10, visitors: 250 }, 
+          { day: 11, visitors: 175 }, 
+          { day: 12, visitors: 200 },
         ]
 
         let visitorsGraphData = $scope.visitorsData
           let data = []
           let num = 100
 
-          // visitorsGraphData.map((e) => {
-          //   data.push(e.day)
-          //   num.push(e.visitors)
-          // })      
+          visitorsGraphData.map((e) => {
+            data.push([e.day, e.visitors])
+        
+          })
+
+             
 
         var margin = {
-          top: 40,
-          right: 30,
-          bottom: 40,
-          left: 50
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 30
         }
         // var height = document.getElementById('visitorsBar').offsetHeight - 100 - margin.top - margin.bottom;
         // var width = document.getElementById('visitorsBar').offsetWidth - margin.right - margin.left;
         var height = 100;
-        var width = 200;
+        var width = 300;
   
 
         var x = d3.scaleBand()
@@ -82,7 +89,7 @@ angular.module('app')
           .attr("y", 6)
           .attr("dy", ".71em")
           .style("text-anchor", "end")
-          .text("Student");
+          // .text("Student");
 
         svg.selectAll(".bar")
           .data(data)
@@ -102,26 +109,26 @@ angular.module('app')
             if (d[1] >= num) return '#21AAE1'
             else return '#141414';
           })
-          .on('mouseover', function (d) {
-            tip.show(d)
-            if (d[1] >= num) {
-              d3.select(this)
-                .attr("fill", "#297FAA");
-            } else {
-              d3.select(this)
-                .attr("fill", "#000");
-            }
-          })
-          .on('mouseout', function (d) {
-            tip.hide(d)
-            if (d[1] >= num) {
-              d3.select(this)
-                .attr("fill", '#21AAE1');
-            } else {
-              d3.select(this)
-                .attr("fill", "#141414");
-            }
-          })
+          // .on('mouseover', function (d) {
+          //   tip.show(d)
+          //   if (d[1] >= num) {
+          //     d3.select(this)
+          //       .attr("fill", "#297FAA");
+          //   } else {
+          //     d3.select(this)
+          //       .attr("fill", "#000");
+          //   }
+          // })
+          // .on('mouseout', function (d) {
+          //   tip.hide(d)
+          //   if (d[1] >= num) {
+          //     d3.select(this)
+          //       .attr("fill", '#21AAE1');
+          //   } else {
+          //     d3.select(this)
+          //       .attr("fill", "#141414");
+          //   }
+          // })
 
         function type(d) {
           d[1] = +d[1];
@@ -147,26 +154,26 @@ angular.module('app')
           .data(newData)
           .attr("y", height)
           .attr("height", 0)
-          .on('mouseover', function (d) {
-              tip.show(d)
-              if (d[1] >= num) {
-                d3.select(this)
-                  .attr("fill", "#297FAA");
-              } else {
-                d3.select(this)
-                  .attr("fill", "#000");
-              }
-            })
-            .on('mouseout', function (d) {
-              tip.hide(d)
-              if (d[1] >= num) {
-                d3.select(this)
-                  .attr("fill", '#21AAE1');
-              } else {
-                d3.select(this)
-                  .attr("fill", "#141414");
-              }
-            })
+          // .on('mouseover', function (d) {
+          //     tip.show(d)
+          //     if (d[1] >= num) {
+          //       d3.select(this)
+          //         .attr("fill", "#297FAA");
+          //     } else {
+          //       d3.select(this)
+          //         .attr("fill", "#000");
+          //     }
+          //   })
+          //   .on('mouseout', function (d) {
+          //     tip.hide(d)
+          //     if (d[1] >= num) {
+          //       d3.select(this)
+          //         .attr("fill", '#21AAE1');
+          //     } else {
+          //       d3.select(this)
+          //         .attr("fill", "#141414");
+          //     }
+          //   })
 
 
           bars.transition()
